d2eac1a8c610c013fff27944ec82e63b2f1de4d0
Guilherme Sales <guilherme.sales@sap.com>
2024-03-28 09:44:29 -0300

    Executa comandos diferentes para host e manager

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-int main() {
+int main(int argc, const char *argv[]) {
 	
 	std::cout << "Hello World" << std::endl;
+
+	if (argc > 1) {
+		std::cout << "I am a " << argv[1] << std::endl;
+	}
 
 	return 0;
 }
